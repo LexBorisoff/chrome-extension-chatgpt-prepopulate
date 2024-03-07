ca9c367033ccf29bda6a44bc109b4a8b36d969ad
Lex <hello@lexborisoff.com>
2024-03-07 14:46:45 -0500

    added claude ai handler

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (
     changeInfo.status === "complete" &&
     tab.status === "complete" &&
-    tab.url != null &&
-    tab.url.startsWith("https://chat.openai.com/")
+    tab.url != null
   ) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      function: checkURL,
-    });
+    if (tab.url.startsWith("https://chat.openai.com/")) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        function: handleChatGPT,
+      });
+      return;
+    }
+
+    if (tab.url.startsWith("https://claude.ai/chats")) {
+      console.log("here");
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        function: handleClaude,
+      });
+    }
   }
 });
 
-function checkURL() {
+function handleChatGPT() {
   const ta = document.querySelector("#prompt-textarea");
   const btn = document.querySelector('[data-testid="send-button"]');
 
@@ -25,4 +35,22 @@ function checkURL() {
     ta.dispatchEvent(inputEvent);
     btn.click();
   }, 0);
+}
+
+function handleClaude() {
+  const fieldset = document.querySelector("fieldset");
+
+  if (fieldset != null) {
+    const p = fieldset.querySelector("p");
+    const btn = fieldset.querySelector("button");
+
+    const url = new URL(window.location.href);
+    const prepopulate = url.searchParams.get("prepopulate");
+
+    setTimeout(() => {
+      console.log(prepopulate);
+      p.innerText = prepopulate;
+      btn.click();
+    }, 0);
+  }
 }
